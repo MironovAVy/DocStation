ba64854bd393a82449eb942ba6db0409a02e8879
MironovAVy <fingertipspeople@gmail.com>
2024-07-07 15:45:52 +0900

    preMigration

---
+++ DocStation.Data.Migrations/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using DocStation.Data.Models;
+
+namespace DocStation.Data.Migrations
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddDbContext<ModelsDBContecx>(options =>
+                        options.UseSqlServer("YourConnectionStringHere"));
+                });
+    }
+}
---
+++ DocStation.Data/Models/ModelsDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocStation.Data.Models
+{
+    public class ModelsDBContecx : DbContext
+    {
+
+        public DbSet<HDepartments> HDepartments { get; set; }
+        public DbSet<HPositions> HPositions { get; set; }
+        public DbSet<HSubsidiaries> HSubsidiaries { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("YourConnectionStringHere");
+        }
+        public ModelsDBContecx(DbContextOptions<ModelsDBContecx> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
