4d632b61943aad05781a6903b00951d703e8bbd9
MironovAVy <fingertipspeople@gmail.com>
2024-07-21 20:10:43 +0900

    TODO finished

--- DocStation.Api/Services/IDepartmentService.cs
+++ DocStation.Api/Services/IDepartmentService.cs
@@ -8,17 +8,25 @@ namespace DocStation.Api.Services
     public class DepartmentService : IDepartmentService 
     {
         private readonly IDictionary<int, HDepartments> _storage = new ConcurrentDictionary<int, HDepartments>();
+        private int _currentId = 0;
 
-        public void Add(HDepartments department)
+        public int Add(HDepartments department)
         {
-			//ToDo: implement method, add entity to _storage
-			throw new NotImplementedException();
+            //ToDo: implement method, add entity to _storage Метод Add отдавал id добавленного департмента и этот Id был уникален
+            if (department == null)
+            {
+                throw new ArgumentNullException(nameof(department));
+            }
+            int newId = ++_currentId;
+            department.Id = newId;
+            _storage[newId] = department;
+            return newId;
         }
 
         public IReadOnlyCollection<HDepartments> GetAll()
         {
-			//ToDo: implement method, get all entities from storage
-			throw new NotImplementedException();
+            //ToDo: implement method, get all entities from storage
+            return new List<HDepartments>(_storage.Values);
         }
     }
     public interface IDepartmentService
