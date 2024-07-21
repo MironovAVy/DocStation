007be8443e3487d94c41da3c8c0f35461b9b1fef
MironovAVy <fingertipspeople@gmail.com>
2024-07-21 15:15:10 +0900

    add AutoMapper to DepartmentsController

--- DocStation.Api/Controllers/DepartmentsController.cs
+++ DocStation.Api/Controllers/DepartmentsController.cs
@@ -2,40 +2,59 @@
 using DocStation.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Xml.Linq;
+using AutoMapper;
+
+using AutoMapper.Execution;
 
 namespace DocStation.Api.Controllers
 {
-    [Route("[controller]")]
-    [ApiController]
+	public class AppMappingProfile : Profile
+	{
+		public AppMappingProfile()
+		{
+			CreateMap<HDepartments, DepartmentsDto>().ReverseMap();
+			
 
-    public class DepartmentsController : ControllerBase
-    {
-        private readonly IDepartmentService _departmentService;
-        public DepartmentsController(IDepartmentService departmentService)
-        {
-            _departmentService = departmentService;
-        }
-        [HttpGet]
 
-        public ActionResult<IReadOnlyCollection<DepartmentsDto>> GetDepartments()
-        {
-            var departments = _departmentService.GetAll();
-			//ToDo: convert from HDepartments[] to DepartmentsDto[]
-            var departmentsDtos = ...
-			return Ok(departmentsDtos);
         }
 
+		
 
-        [HttpPost]
-        
-        public ActionResult AddDepartment([FromBody] NewDepartmentsDto newDepartmentsDto)
-        {
-            //ToDo: Convert from NewDepartmentsDto to HDepartments
-            var newDepartment = ...;
-			_departmentService.Add(newDepartment);
-            return Ok();
-        }
     }
-    public record DepartmentsDto(int Id = default, string Name = default, string Description = default);
-    public record NewDepartmentsDto(string Name = default, string Description = default);
+	
+    [Route("[controller]")]
+	[ApiController]
+
+	public class DepartmentsController : ControllerBase
+	{
+		private readonly IDepartmentService _departmentService;
+		private readonly IMapper _mapper;
+		public DepartmentsController(IDepartmentService departmentService)
+		{
+			_departmentService = departmentService;
+			_mapper = _mapper;
+		}
+		[HttpGet]
+
+		public ActionResult<IReadOnlyCollection<DepartmentsDto>> GetDepartments()
+		{
+			var departments = _departmentService.GetAll();
+			//ToDo: convert from HDepartments[] to DepartmentsDto[]
+			var departmentsDtos = _mapper.Map<IReadOnlyCollection<DepartmentsDto>>(departments);
+			return Ok(departmentsDtos);
+		}
+
+
+		[HttpPost]
+		
+		public ActionResult AddDepartment([FromBody] NewDepartmentsDto newDepartmentsDto)
+		{
+			//ToDo: Convert from NewDepartmentsDto to HDepartments
+			var newDepartment = _mapper.Map<HDepartments>(newDepartmentsDto);
+			_departmentService.Add(newDepartment);
+			return Ok();
+		}
+	}
+	public record DepartmentsDto(int Id = default, string Name = default, string Description = default);
+	public record NewDepartmentsDto(string Name = default, string Description = default);
 }
