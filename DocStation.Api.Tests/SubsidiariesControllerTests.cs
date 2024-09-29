ef043eff601a4c996f85bbcfeb825b9cdcc07b46
MironovAVy <fingertipspeople@gmail.com>
2024-09-29 17:08:31 +0900

    Add AddSubsidiary_SpecifiedDepartmentExist_ReturnsOkStatusCode() Test

--- DocStation.Api.Tests/SubsidiariesControllerTests.cs
+++ DocStation.Api.Tests/SubsidiariesControllerTests.cs
@@ -95,12 +95,45 @@ namespace DocStation.Api.Tests
             _departmentService.Verify(s => s.GetByIdAsync(departmentId), Times.Exactly(1));
         }
 
-
         [Test]
         public async Task AddSubsidiary_SpecifiedDepartmentExist_ReturnsOkStatusCode()
         {
-            //How to setup mock return value
-            Assert.Fail("Not implemented");
+            // Arrange
+            const int departmentId = 101;
+            var newSubsidiaryDto = new NewSubsidiariesDto()
+            {
+                Name = "New Subsidiary",
+                Description = "New Subsidiary Description",
+                DepartmentId = departmentId
+            };
+
+            var department = new HDepartments
+            {
+                Id = departmentId,
+                Name = "IT Department"
+            };
+
+            // Настройка мока для департамента
+            _departmentService.Setup(x => x.GetByIdAsync(departmentId)).ReturnsAsync(department);
+
+            // Act
+            var result = await _subsidiariesController.AddSubsidiary(newSubsidiaryDto);
+            var okResult = result as OkObjectResult;
+
+            // Assert
+            Assert.That(okResult, Is.Not.Null);
+            Assert.That(okResult.StatusCode, Is.EqualTo(200));
+
+            // Проверка на вызов добавления дочерней компании с правильными данными
+            _subsidiariesService.Verify(s => s.AddAsync(It.Is<HSubsidiaries>(subs =>
+                subs.Name == newSubsidiaryDto.Name &&
+                subs.Description == newSubsidiaryDto.Description &&
+                subs.DepartmentId == newSubsidiaryDto.DepartmentId
+            )), Times.Once);
+
+            // Проверка, что GetByIdAsync вызван один раз
+            _departmentService.Verify(s => s.GetByIdAsync(departmentId), Times.Once);
         }
+
     }
 }
